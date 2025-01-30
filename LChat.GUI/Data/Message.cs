d03f71467b52a7c62e1d4a32223bba2f739294e1
Perpetuum1101 <danieliusdav@live.com>
2025-01-30 21:07:13 +0200

    Added Data/Message; Small style fix

---
+++ LChat.GUI/Data/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace LChat.GUI.Data;
+
+public record Message(MessageType Type, string Content, DateTime Timestamp);
+
+public enum MessageType 
+{
+    None,
+    LLM,
+    Client,
+    Meta,
+    Error,
+    Log
+}
